e64b86674200f2e1175a887782eb849c0c3213e8
Ajax Dong <i@aajax.top>
2025-08-04 17:28:40 +0800

    Changed mtu

--- src/tun_device.rs
+++ src/tun_device.rs
@@ -19,7 +19,7 @@ impl TunDevice {
             .name(name)
             .address(ip)
             .netmask(netmask)
-            .mtu(1400)
+            .mtu(1350)
             .up()
             .build()
             .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
