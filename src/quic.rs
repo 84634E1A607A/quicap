6d444d66d64981cff7adf49ad5bbe5bf730b2906
Ajax Dong <i@aajax.top>
2025-08-09 13:43:41 +0800

    Build QUIC client & server

--- src/quic.rs
+++ src/quic.rs
@@ -2,7 +2,7 @@ use std::{net::SocketAddr, path::Path, sync::Arc};
 
 use compio::{
     net::UdpSocket,
-    quic::{self, ClientConfig, ServerConfig},
+    quic::{self, ClientConfig, Endpoint, ServerConfig},
     rustls,
 };
 
@@ -31,35 +31,57 @@ impl<'a> QuicBuilder<'a> {
             root,
         })
     }
-    pub fn build(self) -> Result<Quic, Box<dyn std::error::Error>> {
-        use quic::crypto::rustls::QuicClientConfig;
+    pub async fn build(self) -> Result<Quic, Box<dyn std::error::Error>> {
+        use quic::crypto::rustls::{
+            QuicClientConfig as QuicClientCryptoConfig, QuicServerConfig as QuicServerCryptoConfig,
+        };
         use rustls::{
             ClientConfig as TlsClientConfig, RootCertStore, ServerConfig as TlsServerConfig,
             pki_types::{CertificateDer, PrivatePkcs8KeyDer, pem::PemObject},
             server::WebPkiClientVerifier,
-            version::TLS13,
         };
-        let versions = [&TLS13];
         let cert = CertificateDer::from_pem_file(self.cert)?;
         let key = PrivatePkcs8KeyDer::from_pem_file(self.key)?;
         let ca_cert = CertificateDer::from_pem_file(self.root)?;
         let mut ca = RootCertStore::empty();
         ca.add(ca_cert.clone())?;
         let ca = Arc::new(ca);
-        let client = TlsClientConfig::builder()
+        let tls_client_config = TlsClientConfig::builder()
             .with_root_certificates(ca.clone())
             .with_client_auth_cert(vec![cert.clone()], key.clone_key().into())?;
-        let client = ClientConfig::new(Arc::new(QuicClientConfig::try_from(client)?));
+        let quic_client_config = ClientConfig::new(Arc::new(QuicClientCryptoConfig::try_from(
+            tls_client_config,
+        )?));
+        let endpoint_config = quic::EndpointConfig::default()
+            .max_udp_payload_size(1472)?
+            .clone();
+        let client = Endpoint::new(
+            UdpSocket::bind(SocketAddr::new([0, 0, 0, 0].into(), 0)).await?,
+            endpoint_config.clone(),
+            None,
+            Some(quic_client_config),
+        )?;
+
         let chain = vec![cert, ca_cert];
         let verifier = WebPkiClientVerifier::builder(ca).build()?;
-        let server = TlsServerConfig::builder_with_protocol_versions(&versions)
-            .with_client_cert_verifier(verifier);
-        let server = ServerConfig::with_single_cert(chain, key.into())?;
-        todo!()
+        let tls_server_config = TlsServerConfig::builder()
+            .with_client_cert_verifier(verifier)
+            .with_single_cert(chain, key.into())?;
+        let quic_server_config = ServerConfig::with_crypto(Arc::<QuicServerCryptoConfig>::new(
+            tls_server_config.try_into()?,
+        ));
+        let server = Endpoint::new(
+            UdpSocket::bind(self.listen).await?,
+            endpoint_config,
+            Some(quic_server_config),
+            None,
+        )?;
+
+        Ok(Quic { client, server })
     }
 }
 
 pub struct Quic {
-    listen: UdpSocket,
-    peer: quic::Endpoint,
+    client: Endpoint,
+    server: Endpoint,
 }
